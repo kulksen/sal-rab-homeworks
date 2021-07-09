8c4fa24b9782b8e246a920d7bb613a022c108a2c
kulksen <ya.kulkova2636@gmail.com>
2021-07-09 14:31:30 +0300

    Task_Edited

--- task_1/solution.js
+++ task_1/solution.js
@@ -55,7 +55,7 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     let newSumText = newSum + ' ₽';
     // замените склеивание, используемое в newSum Text на шаблонную строку
-    newSum = `newSum + ₽`;
+    newSum = `${newSum} ₽`;
     // Конец решения задания №1.3.
     
     return newSumText;
