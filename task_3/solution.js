59a588b87aef7e9abcfe13723b2d596dbdc47cba
kulksen <ya.kulkova2636@gmail.com>
2021-07-16 09:34:43 +0300

    Task 3 Edited

--- task_3/solution.js
+++ task_3/solution.js
@@ -5,7 +5,7 @@ function renderCartItem(item) {
 
     // product - объект вида {id: id, title: title, count: count, price: price}
 
-    let product = {id: 1, title: 'Пицца', count: 5, price: '500.00'};
+    product = {id: 1, title: 'Пицца', count: 5, price: '500.00'};
 
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
@@ -20,19 +20,19 @@ function renderCartItem(item) {
     // 1. Создайте переменную itemCountText, равную пустой строке
     let itemCountText = '';
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    itemCountText = itemCountText + count;
+    itemCountText = itemCountText + product.count;
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     itemCountText = itemCountText + ' × ';
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    itemCountText = itemCountText + price;
+    itemCountText = itemCountText + product.price;
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     itemCountText = itemCountText + ' ₽ = ' ;
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    let sum = count * price;
+    let sum = product.count * product.price;
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText = itemCountText + sum;
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
-    itemCountText = itemCountText + '  ₽' ;
+    itemCountText = itemCountText + ' ₽' ;
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
